060f93fbefb4d00a849a38191c5f03fa430771ce
ddoskochynskyi <ddoskochynskyi@leobit.com>
2022-06-13 03:19:01 +0300

    Fix code

--- lib/components/catalog/components/custom_selector.dart
+++ lib/components/catalog/components/custom_selector.dart
@@ -1,7 +1,5 @@
-import 'package:florataba_mobile_app/blocs/catalog_bloc/catalog_bloc.dart';
 import 'package:florataba_mobile_app/components/catalog/components/selector_item.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CustomSelector extends StatelessWidget {
   final List<String> selectorTitle;
@@ -11,13 +9,9 @@ class CustomSelector extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     return Container(
       margin: const EdgeInsets.symmetric(vertical: 12),
-      width: MediaQuery
-          .of(context)
-          .size
-          .width,
+      width: MediaQuery.of(context).size.width,
       child: SingleChildScrollView(
           scrollDirection: Axis.horizontal,
           child: Row(
