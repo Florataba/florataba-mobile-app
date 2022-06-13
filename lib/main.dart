8da0c6e107969170733e732517c037147bd7c589
ddoskochynskyi <ddoskochynskyi@leobit.com>
2022-06-13 03:22:29 +0300

    Updated main(added new bloc provider,changed color)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:florataba_mobile_app/api/user/user_api.dart';
+import 'package:florataba_mobile_app/blocs/catalog_bloc/catalog_bloc.dart';
 import 'package:florataba_mobile_app/blocs/login_bloc/login_bloc.dart';
+import 'package:florataba_mobile_app/blocs/navigator_bloc/navigator_bloc.dart';
 import 'package:florataba_mobile_app/blocs/on_off_bloc/on_off_bloc.dart';
+import 'package:florataba_mobile_app/blocs/selector_bloc/selector_bloc.dart';
 import 'package:florataba_mobile_app/components/catalog/catalog_view.dart';
 import 'package:florataba_mobile_app/components/login/login_view.dart';
 import 'package:florataba_mobile_app/components/login/registration_view.dart';
@@ -29,6 +32,17 @@ class MyApp extends StatelessWidget {
             return _bloc;
           }),
           BlocProvider(create: (BuildContext context) => OnOffBloc()),
+          BlocProvider(create: (BuildContext context) => SelectorBloc()),
+          BlocProvider(create: (BuildContext context) {
+            NavigatorBloc _bloc = NavigatorBloc();
+            _bloc.add(const UpdateNavigation(0));
+            return _bloc;
+          }),
+          BlocProvider(create: (BuildContext context) {
+            CatalogBloc _bloc = CatalogBloc();
+            _bloc.add(const LoadCatalog());
+            return _bloc;
+          }),
         ],
         child: MyHomePage(
           isLogged: isLogged,
@@ -44,6 +58,9 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        theme: ThemeData(
+          scaffoldBackgroundColor: Colors.white,
+        ),
         debugShowCheckedModeBanner: false,
         initialRoute: isLogged ? "/home" : "/login",
         routes: {
