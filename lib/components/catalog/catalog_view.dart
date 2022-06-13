fc0151159bd659b83832a58c02cbf2b84473605f
ddoskochynskyi <ddoskochynskyi@leobit.com>
2022-06-13 03:21:44 +0300

    Completed catalog view(with filtering & navigation)

--- lib/components/catalog/catalog_view.dart
+++ lib/components/catalog/catalog_view.dart
@@ -1,6 +1,14 @@
+import 'dart:math';
+
+import 'package:florataba_mobile_app/blocs/catalog_bloc/catalog_bloc.dart';
 import 'package:florataba_mobile_app/blocs/login_bloc/login_bloc.dart';
+import 'package:florataba_mobile_app/components/catalog/components/catalog_filter_input.dart';
+import 'package:florataba_mobile_app/components/catalog/components/catalog_item.dart';
+import 'package:florataba_mobile_app/components/catalog/components/custom_selector.dart';
+import 'package:florataba_mobile_app/components/navigation/bottom_navigation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class CatalogView extends StatelessWidget {
   const CatalogView({Key? key}) : super(key: key);
@@ -8,6 +16,14 @@ class CatalogView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     LoginBloc _loginBloc = BlocProvider.of<LoginBloc>(context);
+    CatalogBloc _catalogBloc = BlocProvider.of<CatalogBloc>(context);
+    List<Color> colorList = [
+      Colors.blue.shade100,
+      Colors.red.shade100,
+      Colors.yellow.shade100,
+      Colors.purple.shade100,
+      Colors.deepOrange.shade100
+    ];
     return BlocListener<LoginBloc, LoginState>(
         bloc: _loginBloc,
         listener: (context, state) {
@@ -16,30 +32,65 @@ class CatalogView extends StatelessWidget {
                 .pushNamedAndRemoveUntil("/login", (route) => false);
           }
         },
-        child: Scaffold(
-            appBar: AppBar(
-              backgroundColor: Colors.black.withOpacity(0.8),
-              automaticallyImplyLeading: false,
-              title: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Text('Florataba'),
-                  InkWell(
-                      onTap: () => _loginBloc.add(const LogoutUserEvent()),
-                      child: const Icon(Icons.logout))
-                ],
-              ),
-            ),
-            body: BlocBuilder<LoginBloc, LoginState>(
-              bloc: _loginBloc,
-              builder: (context, state) {
-
-                return Container(
-                  child: Text((state is SuccessLogin)
-                      ? state.userEmail as String
-                      : "ERROR"),
-                );
-              },
-            )));
+        child: BlocBuilder<CatalogBloc, CatalogState>(
+          bloc: _catalogBloc,
+          builder: (context, catalogsState) {
+            return Scaffold(
+                bottomNavigationBar: const BottomNavigation(),
+                appBar: AppBar(
+                    backgroundColor: Colors.white,
+                    automaticallyImplyLeading: false,
+                    elevation: 0.0,
+                    title: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: const [
+                          Text('Florataba',
+                              style: TextStyle(color: Colors.black)),
+                        ])),
+                body: SizedBox(
+                  height: MediaQuery.of(context).size.height,
+                  child: Column(
+                    children: [
+                      const CatalogFilterInput(),
+                      const CustomSelector(selectorTitle: [
+                        "All",
+                        "Single",
+                        "Wedding",
+                        "Birthday",
+                        "Valentine's Day",
+                        "Funeral"
+                      ]),
+                      Expanded(
+                        child: Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 16.0),
+                            child: (catalogsState is CatalogLoaded)
+                                ? MasonryGridView.count(
+                                    crossAxisCount: 2,
+                                    mainAxisSpacing: 8,
+                                    crossAxisSpacing: 8,
+                                    itemCount: catalogsState.dataList.length,
+                                    itemBuilder:
+                                        <BuildContext, int>(context, index) {
+                                      return CatalogItem(
+                                          index: index,
+                                          backgroundColor: colorList[colorList
+                                                  .length -
+                                              1 -
+                                              Random()
+                                                  .nextInt(colorList.length)],
+                                          data: catalogsState.dataList[index],
+                                          extent: (index.isEven ? 0.35 : 0.45) *
+                                              MediaQuery.of(context)
+                                                  .size
+                                                  .height);
+                                    })
+                                : const SizedBox()),
+                      ),
+                    ],
+                  ),
+                ));
+          },
+        ));
   }
 }
